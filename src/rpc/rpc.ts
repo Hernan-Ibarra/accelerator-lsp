1f53f9b72a174df34a0fcaee2b22a74288abdfbe
Hernan-Ibarra <hernan.ibarramejia87@thg.com>
2025-02-09 15:45:12 +0000

    Replaced an unwarranted 'any'

--- src/rpc/rpc.ts
+++ src/rpc/rpc.ts
@@ -1,18 +1,20 @@
-export const encodeMessage = (msg: any): string => {
+export const encodeMessage = (msg: Record<string, unknown>): string => {
   const content = JSON.stringify(msg);
   const contentLength = content.length;
   return `Content-Length: ${contentLength}\r\n\r\n${content}`;
-}
+};
 
 export interface BasicMessage {
-  method: string
+  method: string;
 }
 
 export const decodeMessage = (msg: string): BasicMessage => {
   const separated: string[] = msg.split("\r\n\r\n", 2);
 
   if (separated.length !== 2) {
-    throw new Error("There was a problem when splitting the message (maybe the separator wasn't found?)");
+    throw new Error(
+      "There was a problem when splitting the message (maybe the separator wasn't found?)",
+    );
   }
 
   const header = separated[0];
@@ -23,9 +25,9 @@ export const decodeMessage = (msg: string): BasicMessage => {
 
   const baseMessage = JSON.parse(content.slice(0, contentLength));
 
-  if (typeof baseMessage.method !== 'string') {
+  if (typeof baseMessage.method !== "string") {
     throw new Error("The 'method' property is missing or is not a string.");
   }
 
   return baseMessage;
-}
+};
